369355fce81666e59664d07fabbbe637840987e8
Justin Cano <jcano001@ucr.edu>
2014-10-21 15:49:03 -0700

    implemented loadBoard, printBoard, and validateBoard methods

--- sudoku.py
+++ sudoku.py
@@ -20,16 +20,77 @@
 #
 import csv
 
-file = 'sample_input.csv'
+filename = 'bad_input.csv'#'sample_input.csv'
 
-board = []
+def loadBoard(filename):
+    '''
+    Loads the game board with the values contained
+    in the csv file designated by filename
+    Params: filename - the filename of the corresponding
+            csv file to be read
+    Return: board - a game board loaded with the elements
+            from the csv file
+    '''
+    board = []
+    try:
+        with open(filename) as csvfile:
+            reader = csv.reader(csvfile)
+            for row in reader:
+                board.append(row)
+    except IOError:
+        print "IOError: no such file '%s'" % filename
+        loadBoard(raw_input('Please input a valid csv file: '))
 
-with open(file, 'rb') as csvfile:
-    reader = csv.reader(csvfile)
-    for row in reader:
-        board.append(row)
+    return board
 
-for i in range(len(board)):
-    for j in range(len(board)):
-        print board[i][j],
-    print
+def validateBoard(board):
+    '''
+    Checks for validity of the game board loaded from the input
+    csv file. Game board must be a 9x9 element array with each
+    element value between 0-9
+    Params: board - the game board to be validated
+    Return: True if board is valid (9x9 with elements between 0-9)
+            False if board is invalid
+    '''
+    if len(board) != 9:
+        return False
+    for i in range(9):
+        if len(board[i]) != 9:
+            return False
+        for j in range(9):
+            if board[i][j] < 0 or board[i][j]  > 9:
+                return False
+
+
+    return True
+
+
+def printBoard(board):
+    '''
+    Prints the contents of the game board in a human readable
+    format
+    Params: board - the game board to be printed
+    '''
+    print '_' * (len(board)-1) * 3
+    for i in range(len(board)):
+        print '|',
+        for j in range(len(board)):
+            print board[i][j],
+            if (j+1) % 3 == 0:
+                print '|',
+        print
+        if (i+1) % 3 == 0:
+            print '_' * (len(board)-1) * 3
+
+
+def main():
+    #filename = raw_input()
+    board = loadBoard(filename)
+    #printBoard(board)
+    print validateBoard(board)
+
+
+
+
+if __name__ == "__main__":
+    main()
