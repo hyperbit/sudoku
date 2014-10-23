47083cb3ced345742c91e6b75ce42ad01ac20ad2
Justin Cano <jcano001@ucr.edu>
2014-10-22 23:57:41 -0700

    Print invalid input messages

--- sudoku.py
+++ sudoku.py
@@ -20,6 +20,7 @@
 #
 import csv
 import copy
+import sys
 
 filename = 'bad_input.csv'#'sample_input.csv'
 
@@ -195,13 +196,40 @@ def outputToCSV(board):
         for row in board:
             solution.write(','.join(map(str,row))+'\n')
 
+def printNoInputMessage():
+    print "Invalid input!"
+    print "Usage: python sudoku.py <filename.csv>"
 
+def printBadInputMessage():
+    print
+    print "Invalid input!"
+    print """CSV file should be a 9x9 comma separated integer grid,
+with each integer between 0-9 (0 represents blanks), e.g.,
+
+0,3,5,2,9,0,8,6,4
+0,8,2,4,1,0,7,0,3
+7,6,4,3,8,0,0,9,0
+2,1,8,7,3,9,0,4,0
+0,0,0,8,0,4,2,3,0
+0,4,3,0,5,2,9,7,0
+4,0,6,5,7,1,0,0,9
+3,5,9,0,2,8,4,1,7
+8,0,0,9,0,0,5,2,6
+"""
 
 def main():
     board = []
-    while not isValidSudokuBoard(board):
-        filename = raw_input('Enter csv file to load: ')
-        board = loadBoard(filename)
+
+    if len(sys.argv) != 2:
+        printNoInputMessage()
+        return
+
+    filename = sys.argv[1]
+    board = loadBoard(filename)
+    
+    if not isValidSudokuBoard(board):
+        printBadInputMessage()
+        return
 
     printBoard(board)
 
